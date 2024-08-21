616918349d4fd0c6e63239ab7c34538aa351ad27
Joey 007 <PanHaiWJ@163.com>
2024-08-21 13:54:44 +0800

    Turn on debugging temporarily

--- homie/settings.py
+++ homie/settings.py
@@ -155,3 +155,21 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Enable logging temporarily in Heroku
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
